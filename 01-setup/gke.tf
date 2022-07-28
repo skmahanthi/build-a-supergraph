f5af2a7e97815145a26f075b9a46ac4f068d5023
Lucas Leadbetter <5595530+lleadbet@users.noreply.github.com>
2022-07-28 11:20:10 -0400

    adding new vpc setup and dev tooling cluster

--- 01-setup/gke.tf
+++ 01-setup/gke.tf
@@ -1,6 +1,8 @@
 module "gke_auth" {
-  source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
-  for_each     = var.demo_stages
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  for_each = {
+    for index, stage in var.demo_stages : stage.name => stage
+  }
   depends_on   = [module.gke]
   project_id   = var.project_id
   location     = module.gke[each.key].location
@@ -8,29 +10,33 @@ module "gke_auth" {
 }
 
 resource "local_file" "kubeconfig" {
-  for_each = var.demo_stages
+  for_each = {
+    for index, stage in var.demo_stages : stage.name => stage
+  }
   content  = module.gke_auth[each.key].kubeconfig_raw
-  filename = "kubeconfig-${each.key}"
+  filename = "kubeconfig-${each.value.name}"
 }
 
 module "gke" {
-  source   = "terraform-google-modules/kubernetes-engine/google"
-  for_each = var.demo_stages
+  source = "terraform-google-modules/kubernetes-engine/google"
+  for_each = {
+    for index, stage in var.demo_stages : stage.name => stage
+  }
 
   project_id                        = var.project_id
-  name                              = "${var.demo_name}-${each.value}"
+  name                              = "${var.demo_name}-${each.value.name}"
   regional                          = true
   region                            = var.project_region
   disable_legacy_metadata_endpoints = true
 
   network           = module.gcp-network[each.key].network_name
   subnetwork        = module.gcp-network[each.key].subnets_names[0]
-  ip_range_pods     = ""
-  ip_range_services = ""
+  ip_range_pods     = "${var.demo_name}-${each.value.name}-pods"
+  ip_range_services = "${var.demo_name}-${each.value.name}-services"
   node_pools = [
     {
-      name         = "${each.value}-node-pool"
-      machine_type = var.gke_node_type
+      name         = "${each.value.name}-node-pool"
+      machine_type = each.value.node_type
       min_count    = 1
       max_count    = var.gke_num_nodes
       disk_size_gb = 20
