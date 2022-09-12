d0d890ecce0f5e2ef4bc385dbe4c0864a6ad0ff5
Lucas Leadbetter <5595530+lleadbet@users.noreply.github.com>
2022-09-12 11:46:31 -0400

    reverting managed prometheus change due to increased setup time

--- 01-setup/gke.tf
+++ 01-setup/gke.tf
@@ -14,18 +14,16 @@ module "gke_auth" {
 }
 
 module "gke" {
-  source = "terraform-google-modules/kubernetes-engine/google//modules/beta-public-cluster"
+  source = "terraform-google-modules/kubernetes-engine/google"
   for_each = {
     for index, stage in var.demo_stages : stage.name => stage
   }
 
-  project_id                           = var.project_id
-  name                                 = "${var.demo_name}-${each.value.name}"
-  regional                             = true
-  region                               = var.project_region
-  disable_legacy_metadata_endpoints    = true
-  kubernetes_version                   = "1.23" # Bug w/ 1.24: https://github.com/hashicorp/terraform-provider-kubernetes/pull/1792
-  monitoring_enable_managed_prometheus = true
+  project_id                        = var.project_id
+  name                              = "${var.demo_name}-${each.value.name}"
+  regional                          = true
+  region                            = var.project_region
+  disable_legacy_metadata_endpoints = true
 
   network           = module.gcp-network[each.key].network_name
   subnetwork        = module.gcp-network[each.key].subnets_names[0]
--- 01-setup/main.tf
+++ 01-setup/main.tf
@@ -1,7 +1,3 @@
-provider "google-beta" {
-  project = var.project_id
-  region  = var.project_region
-}
 provider "google" {
   project = var.project_id
   region  = var.project_region
