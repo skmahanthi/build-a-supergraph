82266c99b1e014c6163623acf5adc4b229b994de
Lenny Burdette <lenny@apollographql.com>
2022-09-23 10:02:27 -0400

    lots of tweaks/cleanup
    * support different min/max nodes for dev/prod
    * support variable subgraphs
    * clean up docs in .tf files

--- 01-setup/github.tf
+++ 01-setup/github.tf
@@ -2,7 +2,7 @@ provider "github" {
   token = var.github_token
 }
 
-// repositories for k8s KRM/Terraform
+# Infra repo for Router, Otel, load testing
 resource "github_repository" "infra_repo" {
   name        = "${var.demo_name}-infra"
   description = "Apollo K8s Supergraph infrastructure repository"
@@ -16,34 +16,27 @@ resource "github_repository" "infra_repo" {
   }
 }
 
-resource "github_repository" "subgraph_repo_a" {
-  name        = "${var.demo_name}-subgraph-a"
-  description = "Apollo K8s Supergraph subgraph source code repository"
+# Repo for subgraphs
+resource "github_repository" "subgraph_repos" {
+  for_each = {
+    for index, subgraph in var.subgraphs : subgraph.name => subgraph
+  }
+
+  name        = "${var.demo_name}-${each.value.name}"
+  description = "Apollo K8s Supergraph ${each.value.name} source code repository"
   visibility  = "public"
   depends_on = [
     module.gke
   ]
   template {
-    owner      = "apollosolutions"
-    repository = "build-a-supergraph-subgraph-a"
+    owner      = each.value.template_repo.owner
+    repository = each.value.template_repo.repository
   }
 }
 
-resource "github_repository" "subgraph_repo_b" {
-  name        = "${var.demo_name}-subgraph-b"
-  description = "Apollo K8s Supergraph subgraph source code repository"
-  visibility  = "public"
-  depends_on = [
-    module.gke
-  ]
-  template {
-    owner      = "apollosolutions"
-    repository = "build-a-supergraph-subgraph-b"
-  }
-}
+### Github -> GKE Serivce Accounts and credentials ###
 
-// GH -> GKE Serivce Accounts and credentials
-// Deployer credentials for subgraph repos
+# "Deployer" credentials for subgraph repos
 resource "google_service_account" "github-deploy-gsa" {
   project      = var.project_id
   account_id   = "${substr(var.demo_name, 0, 12)}-github-deploy-gsa"
@@ -62,7 +55,7 @@ resource "local_file" "github-deploy-key" {
   filename = "${path.module}/github-deploy-key.json"
 }
 
-// Service Admin credentials for infra repo (so it can manage more resources like cluster roles)
+# "Service Agent" credentials for infra repo (so it can manage more resources like cluster roles)
 resource "google_service_account" "github-manage-gsa" {
   project      = var.project_id
   account_id   = "${substr(var.demo_name, 0, 12)}-github-manage-gsa"
@@ -81,52 +74,43 @@ resource "local_file" "github-manage-key" {
   filename = "${path.module}/github-manage-key.json"
 }
 
-// GH Action Secrets
-// subgraph a
-resource "github_actions_secret" "subgraph_a_apollo_graph_secret" {
-  repository      = github_repository.subgraph_repo_a.name
+### GH Action Secrets ###
+
+# subgraphs
+resource "github_actions_secret" "subgraph_apollo_graph_secret" {
+  for_each = {
+    for index, subgraph in var.subgraphs : subgraph.name => subgraph
+  }
+  repository      = github_repository.subgraph_repos[each.key].name
   secret_name     = "APOLLO_GRAPH_ID"
   plaintext_value = var.apollo_graph_id
 }
-resource "github_actions_secret" "subgraph_a_apollo_secret" {
-  repository      = github_repository.subgraph_repo_a.name
+resource "github_actions_secret" "subgraph_apollo_secret" {
+  for_each = {
+    for index, subgraph in var.subgraphs : subgraph.name => subgraph
+  }
+  repository      = github_repository.subgraph_repos[each.key].name
   secret_name     = "APOLLO_KEY"
   plaintext_value = var.apollo_key
 }
-resource "github_actions_secret" "subgraph_a_cluster_prefix" {
-  repository      = github_repository.subgraph_repo_a.name
+resource "github_actions_secret" "subgraph_cluster_prefix" {
+  for_each = {
+    for index, subgraph in var.subgraphs : subgraph.name => subgraph
+  }
+  repository      = github_repository.subgraph_repos[each.key].name
   secret_name     = "CLUSTER_PREFIX"
   plaintext_value = var.demo_name
 }
-resource "github_actions_secret" "subgraph_a_gcp_secret" {
-  repository      = github_repository.subgraph_repo_a.name
+resource "github_actions_secret" "subgraph_gcp_secret" {
+  for_each = {
+    for index, subgraph in var.subgraphs : subgraph.name => subgraph
+  }
+  repository      = github_repository.subgraph_repos[each.key].name
   secret_name     = "GCP_CREDENTIALS"
   plaintext_value = base64decode(google_service_account_key.github-deploy-key.private_key)
 }
 
-// subgraph b
-resource "github_actions_secret" "subgraph_b_apollo_graph_secret" {
-  repository      = github_repository.subgraph_repo_b.name
-  secret_name     = "APOLLO_GRAPH_ID"
-  plaintext_value = var.apollo_graph_id
-}
-resource "github_actions_secret" "subgraph_b_apollo_secret" {
-  repository      = github_repository.subgraph_repo_b.name
-  secret_name     = "APOLLO_KEY"
-  plaintext_value = var.apollo_key
-}
-resource "github_actions_secret" "subgraph_b_cluster_prefix" {
-  repository      = github_repository.subgraph_repo_b.name
-  secret_name     = "CLUSTER_PREFIX"
-  plaintext_value = var.demo_name
-}
-resource "github_actions_secret" "subgraph_b_gcp_secret" {
-  repository      = github_repository.subgraph_repo_b.name
-  secret_name     = "GCP_CREDENTIALS"
-  plaintext_value = base64decode(google_service_account_key.github-deploy-key.private_key)
-}
-
-// infra
+# infra repo secrets: the only different value is GCP_CREDENTIALS
 resource "github_actions_secret" "infra_apollo_graph_secret" {
   repository      = github_repository.infra_repo.name
   secret_name     = "APOLLO_GRAPH_ID"
